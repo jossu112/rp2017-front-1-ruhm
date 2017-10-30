2c53ffb33321dda3f09de8e83160390be72a4f54
Romil Rõbtšenkov <romilrobtsenkov@users.noreply.github.com>
2017-10-30 16:52:08 +0200

    added sending token with requests

--- src/utils/Api.js
+++ src/utils/Api.js
@@ -3,13 +3,19 @@ import axios from 'axios'
 const BASE_URL = '/api'
 
 export default (method, url, query) => {
+
+  const token = localStorage.jwt
+
   return axios
     .request({
       method: method,
       baseURL: BASE_URL, // NB `baseURL` will be prepended to `url` unless `url` is absolute.
       url: url,
       data: query.data || {}, // PUT, POST, PATCH body
-      params: query.params || {} // url params
+      params: query.params || {}, // url params
+      headers: token 
+        ? { Authorization: 'Bearer ' + token }
+        : {}
     })
     .then(response => {
       return Promise.resolve(response.data)
